7bdffa9cdf5b22ee9aefcee7e2448df046a4530b
Damodharan J <damojay@gmail.com>
2021-08-21 22:01:36 +0530

    Update serverless.js

--- api/serverless.js
+++ api/serverless.js
@@ -1,9 +1,9 @@
 const { convert } = require('convert-svg-to-png');
 
 
-module.exports = async (req, res) => {
-  const png = await convert(req.body);
- 
-  res.set('Content-Type', 'image/png');
-  res.send(png);
+module.exports = (req, res) => {
+  convert(req.body).then(png => {
+    res.set('Content-Type', 'image/png');
+    res.send(png);
+  });
 };
